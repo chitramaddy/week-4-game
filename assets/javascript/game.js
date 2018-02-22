19130a5a6d11f4420e2b74f451d01f351c5bb4cf
Your Name <you@example.com>
2018-02-22 13:27:40 -0500

    changed html layout. brought the crystals and scores to the top of the page.

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+/*variables targetScore, wins, losses, crystalArray[];, totalScore,
+
+    //DOM elements for all the variables
+
+    //newGame()
+    when the start is pressed, hide it;
+     
+
+    game is running = true;
+
+    //Computer pick random number and store in target score
+    //write the target score on DOM
+
+    //for loop through the crystalArray and assign random number between 1-12 to each of them
+    var value = random number
+
+    //on Click, take their value add it to the total score and check if computer score=total score, win ++
+    computerscore<totalscore, losses++
+
+    game is running = false
+
+    empty the values picked for the targetScore, gems
+    show the start button
+    alert the user to click start to begin a new game
